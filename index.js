d82c6c7ee5db959e30c715ec5bb910054917514d
ashwakshaik14 <ashwakshaik15@gmail.com>
2025-03-23 04:17:30 +0530

    first commit

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin:(origin,callback)=>{
-    if(!origin || allowedOrigins.includes(origin)){
-      callback(null,true);
-  }
-  else{
-    callback(new Error("Not allowed by CORS"));
-  }
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log(`Blocked by CORS: ${origin}`);
+      callback(new Error("Not allowed by CORS"));
+    }
   },
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type", "Authorization"], // Added Authorization for API authentication
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}))
+  credentials: true, // Allows sending cookies with requests
+}));
+
 app.use(express.json());
 
 mongoose
